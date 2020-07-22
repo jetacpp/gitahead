30693b00b42f47f774084afd8c12138bacf141a1
jetacpp <user@user.com>
2020-07-21 23:28:58 -0700

    gitahead 2.6.3

---
+++ Resources/window.lua
@@ -0,0 +1,20 @@
+return {
+  path = {
+    full = false
+  },
+  log = {
+    hide = true
+  },
+  tabs = {
+    submodule = true,
+    repository = true
+  },
+  prompt = {
+    merge = true,
+    revert = true,
+    cherrypick = true,
+    stash = false,
+    directories = true,
+    largeFiles = true
+  }
+}
